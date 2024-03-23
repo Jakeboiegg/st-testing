4bc0900f96e10f8170c2a708cfcc0c851b6c6e38
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-03-23 22:32:17 +0800

    capitalised fluff

--- willhem/willhem.py
+++ willhem/willhem.py
@@ -4,12 +4,29 @@ import random
 from .letters import letters
 
 
+def capitaliser(prompt):
+    prompt = list(prompt)
+    word = []
+
+    for chr in prompt:
+        rand = random.randint(0, 1)
+        if rand == 0:
+            chr = chr.lower()
+        if rand == 1:
+            chr = chr.upper()
+
+        word.append(chr)
+
+    word = "".join(word)
+    return word
+
+
 def random_fluff(length):
     fluff_arr = []
     for _ in range(length):
         fluff_arr.append(random.choice(letters))
 
-    return "".join(fluff_arr)
+    return capitaliser("".join(fluff_arr))
 
 
 def willhem():
@@ -18,6 +35,7 @@ def willhem():
 
     number = st.slider("length of will---em name", 0, highest_value)
 
+    st.write("name")
     with st.container(border=True):
         if number == 0:
             st.write()
