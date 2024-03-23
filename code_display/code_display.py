37308ca6a84e27f6e4a0e3fd35e993d3eb89077f
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-03-23 23:55:26 +0800

    adden willhelm

--- code_display/code_display.py
+++ code_display/code_display.py
@@ -2,12 +2,16 @@ import streamlit as st
 
 from .capitaliser_text import text as capitaliser_text
 from .coursework_text import text as coursework_text
+from .willhem_text import text as willhem_text
 
 
 def code_display():
-    option = st.selectbox("code to display", ("capitaliser", "coursework"))
+    option = st.selectbox("code to display", ("capitaliser", "coursework", "willhem"))
 
-    if option == "capitaliser":
-        st.code(capitaliser_text)
-    elif option == "coursework":
-        st.code(coursework_text)
+    match option:
+        case "capitaliser":
+            st.code(capitaliser_text)
+        case "coursework":
+            st.code(coursework_text)
+        case "willhem":
+            st.code(willhem_text)
