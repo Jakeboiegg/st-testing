9f7a4228c2f333a822923c120f11ed7061356186
Jakeboiegg <jake_ling_kae_eun@s2021.ssts.edu.sg>
2024-03-24 00:03:14 +0800

    Revert "deleted codes"
    This reverts commit 3da888cf495513f8f0d8e86e9b41b9a8212ee7b0.

---
+++ pages/code_display/willhem_text.py
@@ -0,0 +1,50 @@
+text = """
+import streamlit as st
+import random
+
+from .letters import letters
+
+
+def capitaliser(prompt):
+    prompt = list(prompt)
+    word = []
+
+    for chr in prompt:
+        rand = random.randint(0, 1)
+        if rand == 0:
+            chr = chr.lower()
+        if rand == 1:
+            chr = chr.upper()
+
+        word.append(chr)
+
+    word = "".join(word)
+    return word
+
+
+def random_fluff(length):
+    fluff_arr = []
+    for _ in range(length):
+        fluff_arr.append(random.choice(letters))
+
+    return capitaliser("".join(fluff_arr))
+
+
+def willhem():
+    highest_value = 50
+    willhem = ["w", "wm", "wim", "wiem", "wilem", "wilhem", "willhem"]
+
+    number = st.slider("length of will---em name", 0, highest_value)
+
+    st.write("name")
+    with st.container(border=True):
+        if number == 0:
+            st.write()
+
+        elif 1 <= number <= 7:
+            st.write(willhem[number - 1])
+
+        elif 8 <= number <= highest_value:
+            fluff = random_fluff(number - 7)
+            st.write(f"will{fluff}hem")
+"""
